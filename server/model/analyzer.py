fe6ccec889a4d265aea88a7eb6e10caa2b933b1d
dejongyeong <dejong.re2@gmail.com>
2021-12-05 22:03:25 +0000

    save tfidf-vectorizer and api return results in dict format

---
+++ server/model/analyzer.py
@@ -0,0 +1,27 @@
+from django.conf import settings
+import joblib
+import os
+import sklearn.metrics as mtr
+
+
+def classify_sentiment(sentence: str)->dict:
+    """
+    A method that returns the sentiment of the user-defined sentence for django app
+    - .joblib file = model file
+    - .pk file = tfidf_vectorizer file
+    """
+    classifier = joblib.load(os.path.join(settings.BASE_DIR, 'model', 'multinomialnb_no_stopwords.joblib'))
+    vector = joblib.load(os.path.join(settings.BASE_DIR, 'model', 'multinomialnb_no_stopwords_vectorizer.joblib'))
+    feature = vector.transform([sentence])      # must be a list
+    
+    # predict and calculate probability - it will return 1x2 matrix where:
+    # 1st row of predict probability: probability for class 0 negative sentiment
+    # 2nd row of predict probability: probability for class 1 positive sentiment
+    pred = classifier.predict(feature)
+    prob = classifier.predict_proba(feature)
+    
+    results = {
+        'pred': pred[0],
+        'prob': prob[:,pred[0]][0]
+    }
+    return results        # the predicted class and the corresponding probability
--- server/sentimentanalysis/views.py
+++ server/sentimentanalysis/views.py
@@ -2,13 +2,14 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import AnalyzerSerializer
+from model.analyzer import classify_sentiment
 
 class AnalyzerView(APIView):
-    def post(self, request):
+    def post(self, request)->Response:
         # input request example - data: {sentence: today is a good day}
         serializer = AnalyzerSerializer(data=request.data)
         if serializer.is_valid():
-            # TODO: Integrate the ML model here and pass to serializer: might need to change data type accordingly
-            return Response({'status': 'success', 'data': serializer.data}, status=status.HTTP_200_OK)
+            result = classify_sentiment(serializer.data['sentence'])
+            return Response({'status': 'success', 'data': result}, status=status.HTTP_200_OK)
         else:
             return Response({'status': 'error', 'data': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
